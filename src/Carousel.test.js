8bffa4e14d2c9e37afcfb228431dc21dc945e53e
Michelle Shirk <michelle.l.shirk@gmail.com>
2022-12-05 20:02:42 -0500

    first commit

---
+++ src/Carousel.test.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+
+it("renders without crashing", function(){
+  render(<Carousel />);
+});
+
+it("matches snapshot", function(){
+  const {asFragment} = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("works when you click on the right arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+});
+
+it("works when you click on the left arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // move backward in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second or third
+  expect(queryByAltText("Photo by Josh Post on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+});
+
+it("hides the left arrow on the first image", function(){
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // expect left arrow to be hidden, but not right arrow
+  expect(queryByTestId("left-arrow")).toHaveClass("hidden");
+  expect(queryByTestId("right-arrow")).not.toHaveClass("hidden");
+});
+
+it("hides the right arrow on the last image", function(){
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // move forward to third image in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  // expect right arrow to be hidden, but not left
+  expect(queryByTestId("right-arrow")).toHaveClass("hidden");
+  expect(queryByTestId("left-arrow")).not.toHaveClass("hidden");
+});
